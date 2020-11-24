f4a0600e38b158b56584a6985a5550f028380660
Michal Jurečko <michal.jurecko@gmail.com>
2020-11-24 09:28:00 +0100

    Add config items

--- src/Configuration/Config.php
+++ src/Configuration/Config.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Keboola\AzureEventHubWriter\Configuration;
 
+use Keboola\AzureEventHubWriter\Exception\InvalidStateException;
 use Keboola\Component\Config\BaseConfig;
 
 class Config extends BaseConfig
@@ -16,5 +17,29 @@ class Config extends BaseConfig
     public function getEventHubName(): string
     {
         return $this->getValue(['parameters', 'hub', 'eventHubName']);
+    }
+
+    public function getTableId(): string
+    {
+        return $this->getValue(['parameters', 'tableId']);
+    }
+
+    public function getMode(): string
+    {
+        return $this->getValue(['parameters', 'mode']);
+    }
+
+    public function hasColumn(): bool
+    {
+        return $this->getMode() === ConfigDefinition::MODE_MESSAGE_COLUMN_VALUE;
+    }
+
+    public function getColumn(): string
+    {
+        if (!$this->hasColumn()) {
+            throw new InvalidStateException('The "column" item is not set.');
+        }
+
+        return $this->getValue(['parameters', 'column']);
     }
 }
---
+++ src/Exception/InvalidStateException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\AzureEventHubWriter\Exception;
+
+class InvalidStateException extends ApplicationException
+{
+
+}
