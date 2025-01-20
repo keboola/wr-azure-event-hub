a93db9d2e6fd69828988510a2361e2f142967d02
MiroCillik <mcillik@gmail.com>
2025-01-20 11:06:04 +0100

    ci debug

--- tests/functional/DatadirTest.php
+++ tests/functional/DatadirTest.php
@@ -21,6 +21,9 @@ class DatadirTest extends DatadirTestCase
         // It is optional, and we want to test if is bad EntityPath specified.
         $connectionString = (string) getenv('CONNECTION_STRING');
         $connectionString = (string) preg_replace('~EntityPath=[^=;]+~', '', $connectionString);
+
+        var_dump($connectionString);
+
         putenv("CONNECTION_STRING_NORMALIZED={$connectionString}");
         putenv("CONNECTION_STRING_BAD_ENTITY_PATH={$connectionString};EntityPath=test");
     }
